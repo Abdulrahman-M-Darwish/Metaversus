1b89a5b616e96f61a45d54834221db50c581d13e
Abdulrahman-M-Darwish <winding192837456456@gmail.com>
2024-03-23 00:31:37 +0200

    fix path issue

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { motion } from "framer-motion";
-import styles from "../../../Metaversus/styles";
+import styles from "@/styles";
 import { navVariants } from "../utils/motion";
 import Image from "next/image";
 
